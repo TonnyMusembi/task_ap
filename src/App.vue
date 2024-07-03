f85ffbfc29326ccdc275768b66133dcfafe6c767
TonnyMusembi <musembitonnj@gmail.com>
2024-07-03 23:31:58 +0300

    add

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,16 @@
 
 <template>
   <div class="text-teal-500">
-   test
+    test
   </div>
-  <div class=" flex justify-items-center text-red-700">
-    color
 
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="relative p-10 w-72 h-36 border-4 border-gray-200 rounded-t-full overflow-hidden">
+      <div
+        class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-52 h-26 bg-gray-300 rounded-t-full flex items-center justify-center text-center">
+        Upload <br />Photo
+      </div>
+    </div>
   </div>
 </template>
 
