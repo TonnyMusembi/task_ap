adfda3851115dd6f68a6cc6cdef5fcc8cf03ab88
Tonny Musembi <tonnymusembi@Tonnys-MacBook-Air.local>
2024-07-23 12:41:55 +0300

    updates

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const handleError = () => {
+  router.push("/tasktwo");
+};
+</script>
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">404</p>
+    <p class="mt-7 text-6xl">Oops!</p>
+    <p class="mt-7">Page Not Found</p>
+    <button class="bg-yellow-500 rounded-md px-1 py-1" @click="handleError">
+      Go Home...
+    </button>
+  </div>
+</template>
